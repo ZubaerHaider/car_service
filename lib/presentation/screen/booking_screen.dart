0a300fd3fef9a2c2f88eddb7eb898c549812a82c
Zubaer Haider <zhefti@gmail.com>
2025-01-11 01:03:10 +0600

    all functionality added

--- lib/presentation/screen/booking_screen.dart
+++ lib/presentation/screen/booking_screen.dart
@@ -1,93 +1,147 @@
-import 'package:car_service/presentation/widget/user_input.dart';
+import 'package:car_service/presentation/widget/custom_field.dart';
 import 'package:flutter/material.dart';
 
-class BookingScreen extends StatelessWidget {
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
+class BookingScreen extends StatefulWidget {
   const BookingScreen({super.key});
 
+  @override
+  State<BookingScreen> createState() => _BookingScreenState();
+}
 
+class _BookingScreenState extends State<BookingScreen> {
+  String dropdownValue = list.first;
 
+  final _formKey = GlobalKey<FormState>();
+
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final emailController = TextEditingController();
+  final makeController = TextEditingController();
+  final modelController = TextEditingController();
+  final yearController = TextEditingController();
+  final plateController = TextEditingController();
+  final titleController = TextEditingController();
+  final startController = TextEditingController();
+  final endController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Details',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 25
-        ),),
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 25
+          ),),
         backgroundColor: Colors.orange,
         centerTitle: true,
       ),
       body:  SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-        
-              const Text('Customer Details',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-              ),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Full name'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Phone'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Email'),
-              const SizedBox(height: 10,),
-        
-              const Text('Car Details',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+          child: Form(
+            key: _formKey,
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                const Text('Customer Details',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Full Name', controller: nameController),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Phone', controller: phoneController),
+
+                const SizedBox(height: 10,),
+                CustomField(label: 'Email', controller: emailController),
+                const SizedBox(height: 10,),
+
+                const Text('Car Details',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Make', controller: makeController),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Model', controller: modelController),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Year', controller: yearController),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Registration Plate', controller: plateController),
+                const SizedBox(height: 10,),
+                const Text('Booking Details',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Title', controller: titleController),
+                const SizedBox(height: 10,),
+                CustomField(label: 'Start Date', controller: startController),
+                const SizedBox(height: 10,),
+                CustomField(label: 'End Date', controller: endController),
+                const SizedBox(height: 10,),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      dropdownValue = value!;
+                    });
+                  },
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
               ),
-        
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Make'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Model'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Year'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Registration Plate'),
-              const SizedBox(height: 10,),
-        
-              const Text('Booking Details',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-        
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Title'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'Start Date'),
-              const SizedBox(height: 10,),
-              const UserInput(text: 'End Date'),
-              const SizedBox(height: 20,),
-              
-              Center(
-                child: ElevatedButton(onPressed: () {},
-                    style:   ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange
-                    ),
-        
-                    child: const Text('Book Now',style: TextStyle(color: Colors.white),)),
-              )
-        
-        
-        
-            ],
+
+                Center(
+                  child: ElevatedButton(onPressed: () {
+                    if(_formKey.currentState!.validate()){
+                      nameController.text;
+                      phoneController.text;
+                    }
+                  },
+                      style:   ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange
+                      ),
+
+                      child: const Text('Book Now',style: TextStyle(color: Colors.white),)),
+                )
+
+
+
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
---
+++ lib/presentation/widget/custom_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+// CustomTextField Widget Class
+class CustomField extends StatefulWidget {
+  final String label;
+  final TextEditingController? controller;
+
+  const CustomField({
+    super.key,
+    required this.label,
+    required this.controller,
+  });
+
+  @override
+  State<CustomField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomField> {
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        filled: true,
+        fillColor: Colors.white,
+
+        label: Text(widget.label) ,
+        hintStyle: const TextStyle(
+            color: Colors.black
+
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Fill the input field';
+        }
+        return null;
+      },
+      style: const TextStyle(
+        color: Colors.black,),
+
+    );
+  }
+}
